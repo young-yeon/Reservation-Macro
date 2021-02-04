68d7f6af4dd18f046f44d6250354389b31fe2edb
young-yeon <as608683@naver.com>
2021-02-04 17:27:30 +0900

    Add func: crawl lists and show them

--- ctrl/ui/main_window.py
+++ ctrl/ui/main_window.py
@@ -1,32 +1,62 @@
 "메인화면 (일정선택~)"
 from datetime import date
+import re
 
 from PyQt5.QtWidgets import QMainWindow
 from PyQt5 import uic
+from selenium.common.exceptions import NoAlertPresentException
 
+from .time_parser import parser
 
 form_class = uic.loadUiType("view/mainWindow.ui")[0]
 
 
 class MainWindow(QMainWindow, form_class):
     "일정 조회하고 결정하는 화면(UI: mainWindow.ui)"
-    def __init__(self, Macro):
+
+    def __init__(self, Macro, parent=None):
         self.macro = Macro
-        super().__init__()
+        super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.progressBar.hide()
         self.set_today()
+        self.searchButton.clicked.connect(self.search_time)
+        # 예약 가능 시간 리스트
+        self.data = list()
+        # 예약 예정 시간
+        # ['연(yyyy)', '월(MM)', '일(dd)', '시(hh)', '분(mm)', '초(ss)']
+        self.target_time = None
 
     def set_today(self):
         "오늘 날짜로 설정"
         today = date.today()
         self.targetDate.setMinimumDate(today)
 
+    def search_time(self):
+        "예약 가능 시간 검색"
+        target = self.targetDate.date().toString("yyyyMMdd")
+        self.macro.driver.get(
+            "https://www.namyeoju.co.kr/Reservation/Reservation.aspx?SelectedDate="+target)
+        self.data = parser(self.macro.driver.page_source)
+        for row in self.data:
+            self.targetList.addItem(row[0])
+        self.targetList.itemClicked.connect(self.load_resv)
+
+    def load_resv(self):
+        "예약 가능 시간 조회"
+        current = self.targetList.currentRow()
+        self.macro.driver.execute_script(self.data[current][1])
+        try:
+            result = self.macro.driver.switch_to_alert()
+            self.target_time = re.findall(r'\d+', result.text)
+            result.accept()
+        except NoAlertPresentException:
+            self.target_time = None
+
 
 if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication
     app = QApplication(__import__('sys').argv)
-    myWindow = MainWindow(0, 0, 0)
+    myWindow = MainWindow(0)
     myWindow.show()
     app.exec_()
-    
---
+++ ctrl/ui/time_parser.py
@@ -0,0 +1,16 @@
+"예약 가능 시간 조회"
+from bs4 import BeautifulSoup
+
+
+def parser(source):
+    "시간만 파싱해서 리스트로 제공하자!"
+    soup = BeautifulSoup(source, 'html.parser')
+    table = soup.select(
+        '#ctl00_ContentPlaceHolder1_UpdPandel > div > div.cnt-right > div > table > tbody > tr')
+    data = list()
+    for tr in table:
+        line = tr.select('td')
+        line = [x.get_text() for x in line[:4]] + [line[4].find('a')['href']]
+        data.append(["\t%s\t|\t%s\t|\t%s" %
+                     (line[0], line[1], line[2]), line[4][11:]])
+    return data
