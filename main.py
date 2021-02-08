1b66d0c9752635d87909187974da70554951153a
young-yeon <as608683@naver.com>
2021-02-08 10:05:27 +0900

    update gui

--- main.py
+++ main.py
@@ -2,10 +2,14 @@
 "reservation app proto type"
 import sys
 from PyQt5.QtWidgets import QApplication, QStackedWidget
+from PyQt5.QtGui import QIcon
 from ctrl import ui
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app.setApplicationName('남여주 예약 도구')
+    app.setWindowIcon(QIcon('view/golf-ball.png'))
+    app.setApplicationVersion('1.0')
     stack = QStackedWidget()
     intro = ui.IntroWindow(stack)
     stack.addWidget(intro)
